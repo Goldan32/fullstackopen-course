fb8b045428c32805c3a1f0f8332c0c61d6affb75
goldan (laptop) <adani32@gmail.com>
2022-01-22 12:55:57 +0100

    part1 - introduction to react finished

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,8 +1,33 @@
 import React from 'react'
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name} you are {props.age} years old.</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Dani';
+  const age = 22;
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Kíra" age={10 + 10}/>
+      <Hello name={name} age={age}/>
+      <Footer />
+    </>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
 
 export default App
